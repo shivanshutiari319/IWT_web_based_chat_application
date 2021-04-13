f4ba3d7a8643a10c05839742cab127d0dbc957c2
shivanshu <shivanshu.tiwari.home@gmail.com>
2021-04-13 18:49:09 +0530

    third

--- chat_application/src/App.js
+++ chat_application/src/App.js
@@ -1,17 +1,26 @@
-import React from 'react'
-import { ChatEngine}  from 'react-chat-engine'
-import './App.css'
-function App() {
-    return (
-        <ChatEngine
-        height="100vh"
-        projectID="d7ab24da-d4ee-4bb2-83ad-c822b22a6a29"
-        userName="shivanshu"
-        userSecret="trisha"
-        
-        />
-      
-    )
-}
+import { ChatEngine } from 'react-chat-engine';
 
-export default App
+import ChatFeed from './Components/ChatFeed';
+import LoginForm from './Components/LoginForm';
+import './App.css';
+
+const projectID = 'd7ab24da-d4ee-4bb2-83ad-c822b22a6a29';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
